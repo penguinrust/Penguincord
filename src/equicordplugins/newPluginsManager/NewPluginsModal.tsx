a6b45912e2b0c0753634fe29d1c0281000d05f16
reden <redenmain@gmail.com>
2026-08-01 02:53:18 +0100

    Update: Fix Plugin Sections

--- src/equicordplugins/newPluginsManager/NewPluginsModal.tsx
+++ src/equicordplugins/newPluginsManager/NewPluginsModal.tsx
@@ -135,7 +135,7 @@ function NewPluginsModal({ modalProps, newPlugins, newSettings }: ModalComponent
                     </BaseText>
                     <br />
                     <Notice.Info className={cl("notice")}>
-                        Equicord is Open Source Software. If you enjoy using it, consider supporting us <Link href="https://github.com/sponsors/thororen1234" target="_blank" rel="noopener noreferrer">here</Link>.
+                        Penguincord is Open Source Software. If you enjoy using it, consider supporting us <Link href="https://github.com/sponsors/thororen1234" target="_blank" rel="noopener noreferrer">here</Link>.
                     </Notice.Info>
                 </>
             }
